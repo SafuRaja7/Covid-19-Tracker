f0c103e86e070927edf9708c3c80e29a380ac4b2
saif-walturn <saif.walturn@gmail.com>
2022-05-20 15:57:31 +0500

    Ui fixes

--- lib/widgets/countries_card.dart
+++ lib/widgets/countries_card.dart
@@ -1,3 +1,4 @@
+import 'package:covid_tracker/configs/app.dart';
 import 'package:covid_tracker/configs/configs.dart';
 import 'package:covid_tracker/models/countries.dart';
 import 'package:flutter/material.dart';
@@ -8,31 +9,34 @@ class CountriesCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: AppDimensions.normalize(30),
-      width: double.infinity,
-      margin: Space.all(0.5, 0.5),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(
-          AppDimensions.normalize(3),
-        ),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black12,
-            offset: Offset(0, 10),
-            blurRadius: 12,
-            spreadRadius: 6.0,
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: AppDimensions.normalize(30),
+        width: double.infinity,
+        margin: Space.all(0.5, 0.5),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(
+            AppDimensions.normalize(3),
           ),
-        ],
-      ),
-      child: Column(
-        children: [
-          Space.y!,
-          Text(country.country!),
-          Space.y1!,
-          Text(country.cases!.toString()),
-        ],
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 10),
+              blurRadius: 12,
+              spreadRadius: 6.0,
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            Space.y!,
+            Text(country.country!),
+            Space.y1!,
+            Text(country.cases!.toString()),
+          ],
+        ),
       ),
     );
   }
